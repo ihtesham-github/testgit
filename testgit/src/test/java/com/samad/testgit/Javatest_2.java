ca3c331e75aaae85bd97d61e3473fc2f3a2d117a
samad <samad_chy@yahoo.com>
2022-10-30 15:56:00 -0500

    Added one code for swapping numbers

--- testgit/src/test/java/com/samad/testgit/Javatest_2.java
+++ testgit/src/test/java/com/samad/testgit/Javatest_2.java
@@ -5,7 +5,16 @@ public class Javatest_2 {
 	public static void main(String[] args) {
 		
 		//Swapping 2 numbers
-
+		int temp;
+		int x= 1746147;
+		int y=566;
+		
+		
+		temp=x;
+		x=y;
+		y=temp;
+		
+		System.out.println(x + "    " +y);
 	}
 
 }
